26ff003210de88d3aa0865590ff342d1ee1d3b6d
ericjauregui <eric.jauregui@ymail.com>
2022-07-04 17:30:31 -0700

    setup init config file

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+
+with open('launch_twitter.bat', 'w') as f:
+    cwd = os.path.abspath(os.curdir)
+    os.chdir(r'..')
+    f.write(
+        os.path.abspath(os.curdir) +
+        r'\.venvs\twitter_bot\Scripts\activate.bat & twitter_bot\main.py')
+
+os.chdir(r'.venvs')
+
+os.system('cmd /c python -m venv --system-site-packages twitter_bot2')
+print(
+    f'Created virtual environment: {os.path.abspath(os.curdir)}\\twitter_bot2')
+
+os.system(r'cmd /c \twitter_bot2\Scripts\activate.bat')
+print('Activated virtual environment')
+print('Installing dependencies...')
+os.chdir(r'..\twitter_bot')
+os.system('cmd /c pip install -r requirements.txt')
